1f759c3d0da7cbb2fd38de0fc4da67365a2252f6
mariale07 <maryalemontoya@gmail.com>
2026-05-21 18:05:02 -0600

    Laboratorio 14
    Subí el laboratorio 14

---
+++ L14_MAMP_1155326/L14_MAMP_1155326/Estudiante.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace L14_MAMP_1155326
+{
+	public class Estudiante
+	{
+		public string nombre;
+		public int edad;
+		public string grado;
+		public decimal[] notas;
+
+		public Estudiante(string nombre, int edad, string grado, decimal[] notas)
+		{
+			this.nombre = nombre;
+			this.edad = edad;
+			this.grado = grado;
+			this.notas = notas; 
+		}
+
+
+		public decimal calcularPromedio()
+		{
+			decimal suma = 0;
+
+			for(int i = 0; i < notas.Length; i++)
+			{
+				suma = suma + notas[i];
+			}
+			decimal promedio = suma / notas.Length;
+
+			return promedio;
+		}
+
+		public void mostrarInformacion()
+		{
+			Console.WriteLine("Nombre: " + nombre);
+			Console.WriteLine("Edad: " + edad);
+			Console.WriteLine("Grado: " + grado);
+
+			Console.WriteLine("Notas: ");
+
+			for (int i = 0; i < notas.Length; i++)
+			{
+				Console.WriteLine(notas[i] + " ");
+			}
+			Console.WriteLine("Promedio: " + calcularPromedio());
+		}
+
+		public void aprobar()
+		{
+			if (calcularPromedio() >= 61)
+			{
+				Console.WriteLine("El estudiante " + nombre + " aprobó");
+			}
+			else
+			{
+				Console.WriteLine("El estudiante " + nombre + " reprobó");
+			}
+		}
+
+		public void agregarNota(decimal nuevaNota)
+		{
+			decimal[] nuevasNotas = new decimal[notas.Length + 1];
+			for (int i=0; i < notas.Length; i++)
+			{
+				nuevasNotas[i] = notas[i];
+			}
+			nuevasNotas[nuevasNotas.Length - 1] = nuevaNota;
+			notas = nuevasNotas;
+			Console.WriteLine("Nueva nota agregada: " + nuevaNota);
+		}
+	}
+}
+
